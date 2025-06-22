7389e0521abe0f8ab4e611e0a4acb3dd6f26c4d0
takaya-maker <takaya.suetake1029@gmail.com>
2025-06-22 19:55:22 +0900

    run.py にあるファイルパス指定について相対パスでは正常に認識が行えなかったため再度修正を行う

--- backend/run.py
+++ backend/run.py
@@ -2,10 +2,25 @@
 import uvicorn
 from app.main import app
 from fastapi.staticfiles import StaticFiles
+import os
 
-app.mount("/static/generated_energy", StaticFiles(directory="../cardscrap/picture/2.Generated_energy"), name="static_generated_energy")
-app.mount("/static/effects", StaticFiles(directory="../cardscrap/picture/3.effect"), name="static_effects")
-app.mount("/static/triggers", StaticFiles(directory="../cardscrap/picture/4.trigger"), name="static_triggers")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+app.mount(
+    "/static/generated_energy",
+    StaticFiles(directory=os.path.join(BASE_DIR, "cardscrap/picture/2.Generated_energy")),
+    name="static_generated_energy"
+)
+app.mount(
+    "/static/effects",
+    StaticFiles(directory=os.path.join(BASE_DIR, "cardscrap/picture/3.effect")),
+    name="static_effects"
+)
+app.mount(
+    "/static/triggers",
+    StaticFiles(directory=os.path.join(BASE_DIR, "cardscrap/picture/4.trigger")),
+    name="static_triggers"
+)
 
 if __name__ == "__main__":
     uvicorn.run(
